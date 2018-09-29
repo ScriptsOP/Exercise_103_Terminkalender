1e364852ea3572b8ba342a282d8776b0d1fd74b5
johannes <suljoc16@new.edvhtl.at>
2018-09-29 12:22:36 +0200

    added some methods

--- Exercise_103_Terminkalender/src/KalenderBL.java
+++ Exercise_103_Terminkalender/src/KalenderBL.java
@@ -7,14 +7,24 @@ public class KalenderBL extends AbstractListModel {
 
     private ArrayList<Termin> termine = new ArrayList<>();
     
+    public void add(Termin t) {
+        termine.add(t);
+        fireIntervalAdded(this, termine.size() - 1, termine.size() - 1);
+    }
+    
+    public void remove(int index) {
+        termine.remove(index);
+        fireContentsChanged(this, 0, termine.size() - 1);
+    }
+    
     @Override
     public int getSize() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return termine.size();
     }
 
     @Override
     public Object getElementAt(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return termine.get(index);
     }
     
 }
