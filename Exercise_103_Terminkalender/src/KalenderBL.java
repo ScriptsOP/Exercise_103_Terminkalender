a1b1ecb369862052f6990538806fd3a4a6a681f5
johannes <suljoc16@new.edvhtl.at>
2018-09-29 12:17:54 +0200

    added some classes

--- Exercise_103_Terminkalender/src/KalenderBL.java
+++ Exercise_103_Terminkalender/src/KalenderBL.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author JoZuG
- */
-public class KalenderBL {
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+
+public class KalenderBL extends AbstractListModel {
+
+    private ArrayList<Termin> termine = new ArrayList<>();
+    
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- Exercise_103_Terminkalender/src/Termin.java
+++ Exercise_103_Terminkalender/src/Termin.java
@@ -1,13 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author JoZuG
- */
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
 public class Termin {
-    
+    private LocalDate day;
+    private LocalDate month;
+    private LocalDate year;
+    private LocalTime hour;
+    private LocalTime minute;
+    private String text;
+
+    public Termin(LocalDate day, LocalDate month, LocalDate year, LocalTime hour, LocalTime minute, String text) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+        this.text = text;
+    }
+
+    public LocalDate getDay() {
+        return day;
+    }
+
+    public LocalDate getMonth() {
+        return month;
+    }
+
+    public LocalDate getYear() {
+        return year;
+    }
+
+    public LocalTime getHour() {
+        return hour;
+    }
+
+    public LocalTime getMinute() {
+        return minute;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s.%s.%s - %s.%s --> %s", getDay(), getMonth(), getYear(), getHour(), getMinute(), getText());
+    }
 }
